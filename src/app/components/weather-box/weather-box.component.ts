dfe0f9c804d4ab1cacbf25d94cad83c889886068
Rasid Redzic <rasid.redzic@mercatis.com>
2020-02-01 10:08:56 +0100

    Add declared module for suppressing error message after JSON import

--- src/app/components/weather-box/weather-box.component.ts
+++ src/app/components/weather-box/weather-box.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as weatherData from "../../core/services/weatherdata.json";
 
 @Component({
-  selector: 'app-weather-box',
-  templateUrl: './weather-box.component.html',
-  styleUrls: ['./weather-box.component.scss']
+  selector: "app-weather-box",
+  templateUrl: "./weather-box.component.html",
+  styleUrls: ["./weather-box.component.scss"]
 })
 export class WeatherBoxComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(weatherData['length']);
   }
-
 }
