dfabaca12415f117eb142930a1b58f17317694b0
Rasid Redzic <rasid.redzic@mercatis.com>
2020-02-01 11:32:28 +0100

    Add dynamic data rendering to question-box - Pass question as props - Add date pipe to timestamp values - Render title in innerHtml - Adjust font-weight for title

--- src/app/components/question-box/question-box.component.ts
+++ src/app/components/question-box/question-box.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { ISearchResultItem } from "app/core/services/search.service";
 
 @Component({
-  selector: 'app-question-box',
-  templateUrl: './question-box.component.html',
-  styleUrls: ['./question-box.component.scss']
+  selector: "app-question-box",
+  templateUrl: "./question-box.component.html",
+  styleUrls: ["./question-box.component.scss"]
 })
 export class QuestionBoxComponent implements OnInit {
+  @Input() question: ISearchResultItem;
+  public renderQuestion: ISearchResultItem;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.renderQuestion = this.question
   }
-
 }
